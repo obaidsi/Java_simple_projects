1ecd943de67f23d0638984ed8b10b275186de8fa
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-07-15 10:35:41 -0700

    Added two new methods in the Temps

--- Temps.java
+++ Temps.java
@@ -1,8 +1,17 @@
 public class Temps{
    public static void main(String[] args){
    }
-   
+   /**
+   * Converts Fahrenheit to Celsius
+   * @param degF degree Fahrenheit
+   * @return degree Celcius
+   */
    public static double fToC(double degF){
       return (degF - 32.0) * 5.0/9.0;
    }
+   
+   public static double cToF(double degC){
+      return degC + 273.15;
+   }
+   
 }
