70e28ee93fec29aedb23437f7a8d38199f5ecaa8
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-07-18 15:44:01 -0700

    CircleArea = Circle Area Calculation for Circle using rectangles is created

---
+++ CircleArea.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CircleArea {
+   public static void main(String[] args){
+      System.out.println("\nCircle Area Calculations Menue\n");
+      System.out.println("1. Set radius");
+      System.out.println("2. Find y values for a given x");
+      System.out.println("3. Calculate actual area");
+      System.out.println("4. Calculate estimated area");
+      System.out.println("5. Exit the program");
+      
+      
+    }
+}
