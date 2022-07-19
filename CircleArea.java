b622cbdaa3fa193161be5bf60580b21b38efa9bd
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-07-18 21:20:19 -0700

    CircleArea functionality is complete only needs initial conditions

--- CircleArea.java
+++ CircleArea.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class CircleArea {
    public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-       Computation cp = new Computation();
-       int choice;
-       double radius = 0, actArea =0.0, estArea=0.0;
+      Scanner sc = new Scanner(System.in);
+      Computation cp = new Computation();
+      int choice;
+      double radius = 0, actArea =0.0, estArea=0.0;
       do {
          System.out.println("\nCircle Area Calculations Menue\n");
          System.out.println("1. Set radius");
@@ -16,37 +16,55 @@ public class CircleArea {
          System.out.println("\nEnter your Choice: ");
          while (!sc.hasNextInt()){
             sc.next();
-            System.out.println("Not a valid Entry. Please renter a valid choice:");
-         }
-     
+            System.out.println("Invalid choice. Enter your choice again:");
+            }
+      
          choice = sc.nextInt();        
          System.out.println("You Entered: "+ choice);
-         
+         while (choice<1 || choice>5){
+            System.out.println("Invalid choice. Enter your choice again:");
+            while (!sc.hasNextInt()){
+               sc.next();
+               System.out.println("Invalid choice. Enter your choice again:");
+               }
+            choice = sc.nextInt();
+            }
          switch(choice){
             case 1: 
-               System.out.println("Enter the radius: ");
-               radius = sc.nextDouble(); 
-               System.out.println("Radius set to " + radius);  
+                radius = getRadius();
                break; 
             case 2:
                System.out.println("Enter x vlaue ");
                double xVal = sc.nextDouble();
-               System.out.println("Y_value = " + cp.calY(xVal, radius));
+               System.out.printf("Y_value = %4.2f", cp.calY(xVal, radius));
                break;
             case 3:
                actArea = cp.actualArea(radius);
-               System.out.println("Actual Area = "+ actArea);
+               System.out.printf("Actual Area = %4.2f", actArea);
                break;
             case 4:
+					radius = getRadius();
                System.out.println("Enter the number of rentangle for estimationi:");
                int numRect = sc.nextInt();
                estArea = cp.estimateArea(numRect, radius);
-               System.out.println("Estimated_Area = "+ estArea);
-               System.out.println("Over_estiamted by "+ cp.areaDeviation(actArea, estArea));
+               System.out.printf("Estimated_Area = %4.2f ", estArea);
+               actArea = cp.actualArea(radius);
+               System.out.printf("Over_estiamted by %%%4.2f", cp.areaDeviation(actArea, estArea));
                break;
-                 }
+            }
          }while (choice != 5);
-            System.out.println("GoodBye");
-
-         }
-}
+      System.out.println("GoodBye");
+   
+      }
+		public static double getRadius(){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the radius: ");
+			double radius = sc.nextInt();
+			while(radius<=0 || radius>1000){
+				System.out.println("Radius must be greater than zero less than 1000. Please renter radius:");
+				radius = sc.nextInt();
+			}
+        	System.out.println("Radius set to " + radius);
+			return radius; 
+		}
+   }
