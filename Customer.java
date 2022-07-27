f0b139c249426947144488446289cfbe8f165c6d
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-07-27 13:35:06 -0700

    Created javaDoc for customer.java

--- Customer.java
+++ Customer.java
@@ -3,22 +3,60 @@ public class Customer {
     private int age;
     private double balance;
 
+    /**
+     * Full Constructor for Customer
+     * @param name name of the customer
+     * @param age   age of the customer
+     * @param balance   balance of the customer
+     */
     public Customer(String name, int age, double balance){
         this.name    = name;
         this.age     = age;
         this.balance = balance;
     }
 
+    /**
+     * Retrieves the customer name
+     * @return  this customer's name
+     */
     public String getName(){return name;}
+
+    /**
+     * Retrieves the customer age
+     * @return  return this customer's age
+     */
     public int getAge(){ return age;}
+
+    /**
+     * Retrieves the customer balance
+     * @return  return this customer's balance
+     */
     public double getBalance(){ return balance;}
 
+    /**
+     * Update this customer's name
+     * @param name  the new name for customer
+     */
     public void setName(String name){ this.name = name; }
+
+    /**
+     * Update this customers age
+     * @param age   the new age for the customer
+     */
 
     public void setAge(int age){ this.age = age; }
 
+    /**
+     * Update the customer's balance
+     * @param balance new balance for the customer
+     */
+
     public void setBalance(double balance){ this.balance = balance; }
 
+    /**
+     * Represent the state of the customer in string form
+     * @return   a string representation of the customer's state
+     */
     public String toString(){
         String custInfo = "";
         custInfo += "Customer name      : " + name + "\n";
